99d968d70698a5523fe0da18ff572257e9b73dab
Dario Mendez Musico <dario.mendez@mindtek.it>
2015-04-09 17:31:54 +0200

    Initial Commit

---
+++ SynapseSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SynapseSample
+//
+//  Created by Dario Mendez Musico on 09/04/15.
+//  Copyright (c) 2015 MindTek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
